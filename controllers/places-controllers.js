8d03736664dfb52084ace5e0817ecd3bfd7045d4
mjanks <mjanks10@gmail.com>
2021-05-19 20:39:16 -0400

    setup first routes, implemented error handling

---
+++ controllers/places-controllers.js
@@ -0,0 +1,68 @@
+const { v4: uuid } = require('uuid');
+
+const HttpError = require('../models/http-error');
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous skscrapers in the world!',
+    location: {
+      lat: 40.748474,
+      lng: -73.9871516,
+    },
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1',
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError('Could not find a place for the provided id.', 404);
+  }
+
+  // If the name of the property is equal to the name of the variable
+  // that holds the value you want to store in the property,
+  // you can shorten it as shown below:
+  res.json({ place }); // => { place } => { place: place }
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return next(
+      new HttpError('Could not find a place for the provided user id.', 404)
+    );
+  }
+
+  res.json({ place });
+};
+
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createdPlace);
+
+  res.status(201).json({ place: createdPlace });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
+const placesControllers = require('../controllers/places-controllers');
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET Request in Places');
-  res.json({ message: 'It works!' });
-});
+router.get('/:pid', placesControllers.getPlaceById);
+
+router.get('/user/:uid', placesControllers.getPlaceByUserId);
+
+router.post('/', placesControllers.createPlace);
 
 module.exports = router;
