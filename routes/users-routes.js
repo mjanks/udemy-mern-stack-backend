799c7f2ed99eb3cd2c4acb97df11511590ae9f36
mjanks <mjanks10@gmail.com>
2021-05-23 18:05:56 -0400

    implemented google geocoding API

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -10,20 +10,11 @@ router.get('/', usersControllers.getUsers);
 router.post(
   '/signup',
   check('name').not().isEmpty(),
-  check('email').isEmail(),
-  check('email').not().isEmpty(),
-  check('password').isStrongPassword(),
-  check('password').not().isEmpty(),
+  check('email').normalizeEmail().isEmail(),
   check('password').isLength({ min: 8 }),
   usersControllers.signup
 );
 
-router.post(
-  '/login',
-  check('email').isEmail(),
-  check('email').not().isEmpty(),
-  check('password').not().isEmpty(),
-  usersControllers.login
-);
+router.post('/login', usersControllers.login);
 
 module.exports = router;
---
+++ util/location.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+const HttpError = require('../models/http-error');
+
+const API_KEY = 'AIzaSyCysT3h9f9Dh17wctD_B-Kl2EG3dcrnFuk';
+
+async function getCoordsForAddress(address) {
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY}`
+  );
+
+  const data = response.data;
+
+  if (!data || data.status === 'ZERO_RESULTS') {
+    const error = new HttpError(
+      'Could not find the location for the specified address.',
+      422
+    );
+    throw error;
+  }
+
+  const coordinates = data.results[0].geometry.location;
+
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
